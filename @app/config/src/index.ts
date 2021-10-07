abb79facbfb9a036bb619ee0942678f5b211ee79
Ludwig Bäcklund <ludwigbacklund@gmail.com>
2021-10-07 09:03:51 +0200

    Remove unused dependencies and configuration

--- @app/config/src/index.ts
+++ @app/config/src/index.ts
@@ -1,7 +1,3 @@
 // @ts-ignore
 const packageJson = require('../../../package.json');
-
-export const fromEmail = '"saas" <no-reply@backlund.dev>';
 export const projectName = packageJson.name.replace(/[-_]/g, ' ');
-export const companyName = projectName; // For copyright ownership
-export const awsRegion = 'eu-west-1';
